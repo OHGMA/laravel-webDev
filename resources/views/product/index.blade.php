32249e32f410185e4a5f28b5607e475e4ebaa204
OHGMA <gantenghnf@gmail.com>
2025-09-17 17:58:45 +0700

    update laravel 12 & add model

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
+</head>
+<body>
+
+    <h1>My Produk</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAMA</th>
+                <th>HARGA</th>
+                <th>STOK</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
